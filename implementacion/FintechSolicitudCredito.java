c541b32b0728289f587550125bf3aa7ac96e0618
juanpagu2101 <guzmanpjpablo@gmail.com>
2023-03-23 08:22:14 -0500

    Creacion de parte A del patron

---
+++ implementacion/FintechSolicitudCredito.java
@@ -0,0 +1,22 @@
+package implementacion;
+
+public class FintechSolicitudCredito {
+    private String Cliente;
+    private double Valor;
+
+    public String getCliente() {
+        return Cliente;
+    }
+
+    public void setCliente(String Cliente) {
+        this.Cliente = Cliente;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setValor(double Valor) {
+        this.Valor = Valor;
+    }
+}
---
+++ patronadapter/PatronAdapterMain.java
@@ -0,0 +1,43 @@
+package patronadapter;
+
+import implementacion.AdaptadorFintechA;
+import implementacion.AdaptadorFintechB;
+import implementacion.FintechCreditoRespuesta;
+import implementacion.FintechSolicitudCredito;
+import implementacion.InterfaceFintechAdaptador;
+
+public class PatronAdapterMain {
+    public static void main(String[] args) {
+        
+        FintechSolicitudCredito request = new FintechSolicitudCredito();
+       request.setCliente("Cristiano Ronaldo");
+       request.setValor(1000);
+
+       InterfaceFintechAdaptador FinancieraB = new AdaptadorFintechB();
+       FintechCreditoRespuesta FBresponse = FinancieraB.EnvioSolicitudCredito(request);
+       System.out.println("Resultado aprobación Financiera <<B>>: > " + FBresponse.isAprobacion() + "\n");
+       
+       if (FBresponse.isAprobacion()) {
+           System.out.println("Felicitaciones!!!!!, la Financiera <<B>> aprobó su solicitud de crédito." + "\n");
+       } 
+       else
+           { 
+            System.out.println("Lo sentimos, la Financiera <<B>> no aprobó su crédito");   
+           }
+           
+       
+       
+       InterfaceFintechAdaptador FinancieraA = new AdaptadorFintechA();
+       FintechCreditoRespuesta FAresponse = FinancieraA.EnvioSolicitudCredito(request);
+       System.out.println("Resultado aprobación Financiera <<A>>: > " + FAresponse.isAprobacion() + "\n");
+       
+       if (FAresponse.isAprobacion()) {
+           System.out.println("Felicitaciones!!!!!, la Financiera <<A>> aprobó su solicitud de crédito.");
+       } 
+       else
+           { 
+            System.out.println("Lo sentimos, la Financiera <<A>> no aprobó su crédito");   
+           }
+       
+   }
+}
